7443884b0770c541eff5f2dcb7e1bc551b6ec090
José Jonathan <josejonathan689@gmail.com>
2021-09-21 20:31:48 -0300

    organização da lista

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text  } from 'react-native';
 
 export default function App() {
-  const [ list, setList ] = useState(['Flamengo', 'Palmeiras', 'Atletico Mineiro', 'São Paulo', 'Internacional', 'Atletico Paranaense', 'Fortaleza', 'America Mineiro', 'Bahia', 'Santos', 'Corinthians', 'Grêmio', 'Bragantino', 'Fluminense', 'Cuiaba', 'Juventude', 'Chapecoense', 'Sport', 'Ceara', 'Atlético Goianiense']);
+  const [ list, setList ] = useState(['America Mineiro', 'Atlético Goianiense', 'Atletico Mineiro', 'Atletico Paranaense', 'Bahia', 'Bragantino', 'Ceara', 'Chapecoense', 'Corinthians', 'Cuiaba', 'Flamengo', 'Fortaleza', 'Fluminense', 'Grêmio', 'Internacional', 'Juventude', 'Palmeiras', 'Santos', 'São Paulo', 'Sport']);
 
   const listData = (list, index) => {
     return (
