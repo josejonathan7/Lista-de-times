3084f711e3d437fb2396df5e9da825ba1c120077
jose jonathan <josejonathan689@gmail.com>
2022-02-07 20:19:33 -0300

    configurando um detalhe

--- App.js
+++ App.js
@@ -1,28 +1,23 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text  } from 'react-native';
+import { View, Text, ScrollView  } from 'react-native';
+import { style } from './style'
 
 export default function App() {
   const [ list, setList ] = useState(['America Mineiro', 'Atlético Goianiense', 'Atletico Mineiro', 'Atletico Paranaense', 'Bahia', 'Bragantino', 'Ceara', 'Chapecoense', 'Corinthians', 'Cuiaba', 'Flamengo', 'Fortaleza', 'Fluminense', 'Grêmio', 'Internacional', 'Juventude', 'Palmeiras', 'Santos', 'São Paulo', 'Sport']);
 
   const listData = (list, index) => {
     return (
-      <Text style={styles.container} key={index}>
-          {list}
-      </Text>
+      
+        <Text style={style.container} key={index}>
+            {list}
+        </Text>
     )
   }
 
   return (
-    <View style={styles.container}>
-        {list.map(listData)}
-    </View>
+    <ScrollView style={style.container}>
+  {list.map(listData)}
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#8A2BE2"
-  },
-});
---
+++ style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+
+export const style = StyleSheet.create({
+    container: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "#8A2BE2",
+        padding: 20
+      },
+})
